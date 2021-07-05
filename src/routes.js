713fb39317748cb9ed118c4dd57a2a60ec9fc791
Vitor Penna <62647884+snaipasan@users.noreply.github.com>
2021-07-05 07:50:10 -0300

    Add files via upload

---
+++ src/routes.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+
+const AppStack = createStackNavigator();
+
+import home from './pages/home';
+import PBarb from './pages/PBarb';
+import PBiblic from './pages/PBiblic';
+import CCP from './pages/CCP';
+import PCond from './pages/PCond';
+import PLoj from './pages/PLoj';
+import CV from './pages/CV';
+import Cont from './pages/Cont';
+import PGarg from './pages/PGarg';
+
+export default function Routes() {
+  return (
+    <NavigationContainer>
+      <AppStack.Navigator screenOptions={{ headerShown: false }} >
+        <AppStack.Screen name="home" component={home} />
+        <AppStack.Screen name="PBarb" component={PBarb} />
+        <AppStack.Screen name="PBiblic" component={PBiblic} />
+        <AppStack.Screen name="CCP" component={CCP} />
+        <AppStack.Screen name="PCond" component={PCond} />
+        <AppStack.Screen name="PLoj" component={PLoj} />
+        <AppStack.Screen name="CV" component={CV} />
+        <AppStack.Screen name="Cont" component={Cont} />
+        <AppStack.Screen name="PGarg" component={PGarg} />
+      </AppStack.Navigator>
+    </NavigationContainer>
+
+  );
+}
